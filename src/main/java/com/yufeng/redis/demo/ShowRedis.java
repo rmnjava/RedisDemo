5603618e30215e92067054a253b6808d19f51031
FanYufeng02 <yufeng.fan02@ele.me>
2018-02-09 13:08:46 +0800

    新增定时扫描redis某个队列中的所有内容

--- src/main/java/com/yufeng/redis/demo/ShowRedis.java
+++ src/main/java/com/yufeng/redis/demo/ShowRedis.java
@@ -1,17 +1,20 @@
 package com.yufeng.redis.demo;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 /**
  * 监控redis中的数据
  */
 public class ShowRedis implements Runnable{
 
-    Jedis jedis=new Jedis("192.168.56.101",6379);
+    JedisPool jedisPool=new JedisPool("10.103.105.26",6379);
+    Jedis jedis=null;
     public void run() {
         while(true){
 
             try{
+                jedis=jedisPool.getResource();
                 System.out.println(jedis.lrange("runningtask",0L,99L));
                 jedis.close();
                 Thread.sleep(5000L);
